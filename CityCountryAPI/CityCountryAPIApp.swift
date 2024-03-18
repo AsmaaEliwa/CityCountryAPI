c59c4a2e4178c8ddbe2e0fca255fa4e0ffd44b40
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-03-18 13:45:19 +0200

    setting up app

---
+++ CityCountryAPI/CityCountryAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityCountryAPIApp.swift
+//  CityCountryAPI
+//
+//  Created by asmaa gamal  on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityCountryAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityCountryAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CityCountryAPI
+//
+//  Created by asmaa gamal  on 18/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
